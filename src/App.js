c24dee46ca679cf424e61607e16e14598673f14b
Sayaka Tomie <saya.tomie@gmail.com>
2023-03-10 20:44:34 +0900

    メモ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import './App.css'
+import { Header } from './Header'
+import { Description } from './Description'
+import { DogListContainer } from './DogListContainer'
+/**
+ *
+ * @type {React.FC}
+ */
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Description />
+      <DogListContainer />
     </div>
-  );
+  )
 }
-
-export default App;
